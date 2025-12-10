459312ef5c9e1004075c00f39cd66c90435237c4
clairewang0620-netizen <clairewang0620@gmail.com>
2025-12-10 22:55:49 +0800

    Create App.vue

---
+++ src/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>French Learning App</h1>
+    <p>count is {{ count }}</p>
+    <button @click="count++">Increment</button>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const count = ref(0)
+</script>
+
+<style scoped>
+div {
+  text-align: center;
+  margin-top: 50px;
+}
+button {
+  padding: 10px 20px;
+  font-size: 16px;
+  cursor: pointer;
+}
+</style>
