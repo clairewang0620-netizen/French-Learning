afc7e8626b7a427ad9cdd81dd54eeeb96a06077b
clairewang0620-netizen <clairewang0620@gmail.com>
2025-12-10 22:46:20 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [vue()],
-  base: './'
+  plugins: [react()],
 })
