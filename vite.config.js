d8a47b087003ce9a58e010c074cd84f7c0eeb873
clairewang0620-netizen <clairewang0620@gmail.com>
2025-12-10 12:58:15 +0800

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+# 在项目根目录运行
+touch vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+    sourcemap: false
+  },
+  server: {
+    port: 3000,
+    open: true
+  }
+})
